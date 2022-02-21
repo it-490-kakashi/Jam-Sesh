402a21528c2363ba464b10d8fe5d541f7fe2b2f9
Ido Tanne <idovader@gmail.com>
2022-02-20 20:26:15 -0500

    Set up basic searching using genius api

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request
 import os
+import requests
 from dotenv import load_dotenv
 from blueprints.crud import crud
 from blueprints.celery_test import celery_test
@@ -18,8 +19,36 @@ def create_db():
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    context = {
+        'title': 'Home Page'
+    }
+    return render_template('base.html', data=context)
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == "GET":
+        context = {
+            'title': 'Search'
+        }
+        return render_template('search.html', title=context['title'])
+
+    if request.method == "POST":
+        context = {
+            'title': 'Search',
+            'results': request_song_info(request.form['search_info']).json(),
+        }
+        return render_template('search.html', title=context['title'], results=context['results'])
+
+
+def request_song_info(search_info):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + os.getenv('GENIUS_AUTH')}
+    search_url = base_url + '/search'
+    data = {'q': search_info}
+    response = requests.get(search_url, data=data, headers=headers)
+
+    return response
 
 if __name__ == '__main__':
     app.run()
