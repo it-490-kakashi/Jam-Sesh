31d2448f3734840da82ffdccaa80d1dc223b41b1
Ido Tanne <idovader@gmail.com>
2022-03-01 13:36:23 -0500

    added new tables and added relationships

--- worker/tasks.py
+++ worker/tasks.py
@@ -4,7 +4,7 @@ from celery import Celery
 from celery.utils.log import get_task_logger
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, exc
-from sqlalchemy import Table, Column, String, MetaData, Integer, Identity
+from sqlalchemy import Table, Column, String, MetaData, Integer, Identity, ForeignKey
 
 load_dotenv()
 
@@ -21,8 +21,24 @@ meta = MetaData(db)
 basic_user = Table('users', meta,
                    Column('id', Integer, Identity(start=1, cycle=True), primary_key=True),
                    Column('first_name', String),
-                   Column('last_name', String)
+                   Column('last_name', String),
+                   Column('email', String),
+                   Column('username', String),
+                   Column('password', String)
                    )
+
+song_list = Table('songs', meta,
+                  Column('id', Integer, Identity(start=1, cycle=True), primary_key=True),
+                  Column('name', String),
+                  Column('artist', String),
+                  Column('genre', String)
+                  )
+
+liked_songs = Table('liked_songs', meta,
+                    Column('id', Integer, Identity(start=1, cycle=True), primary_key=True),
+                    Column('song_id', None, ForeignKey('songs.id')),
+                    Column('user_id', None, ForeignKey('users.id'))
+                    )
 
 
 @app.task()
