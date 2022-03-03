29f0ff09c1f3728f4d9103c3a0870a73cd4c385b
Ido Tanne <idovader@gmail.com>
2022-03-03 11:35:42 -0500

    added user_show function

--- server/app.py
+++ server/app.py
@@ -1,17 +1,17 @@
 from flask import Flask, redirect, render_template, request
-import os
-import requests
 from dotenv import load_dotenv
 from blueprints.crud import crud, create_db
 from blueprints.celery_test import celery_test
 from blueprints.api_calls import api_calls
+from blueprints.users import users
 
 load_dotenv()
 
 app = Flask(__name__)
-app.register_blueprint(crud, url_prefix="/api")
-app.register_blueprint(celery_test, url_prefix="/celery")
-app.register_blueprint(api_calls, url_prefix="")
+app.register_blueprint(crud, url_prefix='/api')
+app.register_blueprint(celery_test, url_prefix='/celery')
+app.register_blueprint(api_calls, url_prefix='')
+app.register_blueprint(users, url_prefix='')
 
 
 @app.before_first_request
