c94a109ad8af6e9abb23f233941ee1ef184962b3
Ido Tanne <idovader@gmail.com>
2022-03-10 20:22:49 -0500

    Added playlist models

--- worker/models.py
+++ worker/models.py
@@ -1,7 +1,6 @@
 from creds import db
 from sqlalchemy import Table, Column, String, MetaData, Integer, Identity, ForeignKey, Sequence
 
-
 # SQL Expression Language
 meta = MetaData(db)
 basic_user = Table('users', meta,
@@ -26,3 +25,14 @@ liked_songs = Table('liked_songs', meta,
                     Column('song_id', None, ForeignKey('songs.genius_id')),
                     Column('user_id', None, ForeignKey('users.id'))
                     )
+
+playlist = Table('playlists', meta,
+                 Column('id', Integer, Identity('playlist_id_seq', start=1, increment=1), primary_key=True),
+                 Column('user_id', None, ForeignKey('users.id')),
+                 Column('playlist_name', String)
+                 )
+
+playlist_content = Table('playlist_content', meta,
+                         Column('playlist_id', None, ForeignKey('playlists.id')),
+                         Column('song_id', None, ForeignKey('songs.genius_id'))
+                         )
