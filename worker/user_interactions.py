effbe0882cc623a7ac91a319eea7468c88f2bfc5
rn273 <rajnayak53181@gmail.com>
2022-03-11 12:49:02 -0500

    Updated login

--- worker/user_interactions.py
+++ worker/user_interactions.py
@@ -2,6 +2,7 @@ from sqlalchemy import exc
 from creds import db
 from models import basic_user
 from find_user import by_username
+import secrets
 
 
 def login(username, password):
@@ -10,12 +11,23 @@ def login(username, password):
             user = basic_user.select().where(basic_user.c.username == username & basic_user.c.password == password)
             if len(user.fetchall()) == 1:
                 info = {
-                    'session_id': 1231241,  # TODO: Generate token
-                    'user_info': {
+                    "session_id": secrets.token_hex(5),  # Generated session id
+                    "user_info": {
                         "username": username,
-                        "full_name": f"{user['first']} {user['last']}"
+                        "password": password
+
                     }
                 }
-                # Generate session id
-                # Compile user info & session id into directory
-                return info
+                # username_data = conn.execute(basic_user.select().where(basic_user.c.username == username).fetchall()
+                # password_data = conn.execute(basic_user.select().where(basic_user.c.password == password).fetchall()
+                # username_data = conn.execute("SELECT username FROM users WHERE username=:username").fetchall()
+                # password_data = conn.execute("SELECT password FROM users WHERE password=:password").fetchall()
+
+                users_data = basic_user.select().where(basic_user.c.username == username and basic_user.c.password == password)
+                result = conn.execute(users_data)
+                if len(result.fetchall() == 0):
+                    return False
+                return True
+
+            else:
+                return "Error!"
