8b22a403e64bfe870e6d7d8acdd065acab08caa9
Ido Tanne <idovader@gmail.com>
2022-03-11 13:02:11 -0500

    Updated models to use Identity

--- worker/models.py
+++ worker/models.py
@@ -1,11 +1,11 @@
 from creds import db
-from sqlalchemy import Table, Column, String, MetaData, Integer, Identity, ForeignKey, Sequence
+from sqlalchemy import Table, Column, String, MetaData, Integer, Identity, ForeignKey
 
 
 # SQL Expression Language
 meta = MetaData(db)
 basic_user = Table('users', meta,
-                   Column('id', Integer, Sequence('user_id_seq'), primary_key=True),
+                   Column('id', Integer, Identity('user_id_seq', start=1, increment=1), primary_key=True),
                    Column('first_name', String),
                    Column('last_name', String),
                    Column('email', String),
@@ -14,14 +14,14 @@ basic_user = Table('users', meta,
                    )
 
 song_list = Table('songs', meta,
-                  Column('id', Integer, Sequence('song_id_seq'), primary_key=True),
+                  Column('id', Integer, Identity('song_id_seq', start=1, increment=1), primary_key=True),
                   Column('name', String),
                   Column('artist', String),
                   Column('genre', String)
                   )
 
 liked_songs = Table('liked_songs', meta,
-                    Column('id', Integer, Sequence('liked_song_id_seq'), primary_key=True),
+                    Column('id', Integer, Identity('liked_song_id_seq', start=1, increment=1), primary_key=True),
                     Column('song_id', None, ForeignKey('songs.id')),
                     Column('user_id', None, ForeignKey('users.id'))
                     )
