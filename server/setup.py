48b5d8a608a42a091c07bf76492fa52833a79e53
Ido Tanne <idovader@gmail.com>
2022-05-04 20:12:21 -0400

    Finalized production deployment

--- server/setup.py
+++ server/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Jam-Sesh-Server',
-    version='2.0.0',
+    version='1.0.0',
     packages=['blueprints'],
     include_package_data=True,
     zip_safe=False
--- worker/setup.py
+++ worker/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Jam-Sesh-Worker',
-    version='2.0.0',
+    version='1.0.0',
     packages=[],
     include_package_data=True,
     zip_safe=False
